cb1f4933789f8bb1d5960889dbf262ee4c464153
has0825 <k024g0026@g.neec.ac.jp>
2025-09-05 11:31:18 +0900

    Title作成

--- Title.h
+++ Title.h
@@ -1,2 +1,35 @@
 #pragma once
-class Title {};
+#include "Fade.h"
+#include <KamataEngine.h>
+
+using namespace KamataEngine;
+
+class TitleScene {
+public:
+	TitleScene() = default;
+	~TitleScene();
+
+	void Initialize();
+	void Update();
+	void Draw();
+
+	// main.cpp からの遷移判定に使う
+	bool IsFinished() const;
+
+private:
+	// ステップ（フェードイン→待機→フェードアウト→終了）
+	enum class Step {
+		FadeIn,
+		Idle,
+		FadeOut,
+		Done,
+	} step_ = Step::FadeIn;
+
+	// 表示物
+	Camera camera_{};
+	Model* modelTitle_ = nullptr;
+	std::unique_ptr<WorldTransform> titleWT_;
+
+	// フェード
+	std::unique_ptr<Fade> fade_;
+};
--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "GameScene.h"
+#include "Title.h"
 #include <KamataEngine.h>
 #include <Windows.h>
+#include <memory>
+
 using namespace KamataEngine;
+
+enum class Scene {
+	Title,
+	Game,
+};
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
@@ -11,8 +19,13 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
-	GameScene* gameScene = new GameScene();
-	gameScene->Initialize();
+	Scene scene = Scene::Title;
+
+	// unique_ptr による安全な管理
+	std::unique_ptr<TitleScene> titleScene = std::make_unique<TitleScene>();
+	titleScene->Initialize();
+
+	std::unique_ptr<GameScene> gameScene = nullptr;
 
 	// メインループ
 	while (true) {
@@ -21,17 +34,44 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 			break;
 		}
 
-		// 更新
-		gameScene->Update();
+		// ====== シーンごとの更新 ======
+		switch (scene) {
+		case Scene::Title:
+			titleScene->Update();
+			if (titleScene->IsFinished()) {
+				titleScene.reset(); // delete不要で自動解放
 
-		// 描画
+				gameScene = std::make_unique<GameScene>();
+				gameScene->Initialize();
+				scene = Scene::Game;
+			}
+			break;
+
+		case Scene::Game:
+			gameScene->Update();
+			break;
+		}
+
+		// ====== 描画 ======
 		dxCommon->PreDraw();
-		gameScene->Draw();
+		switch (scene) {
+		case Scene::Title:
+			if (titleScene) {
+				titleScene->Draw();
+			}
+			break;
+		case Scene::Game:
+			if (gameScene) {
+				gameScene->Draw();
+			}
+			break;
+		}
 		dxCommon->PostDraw();
 	}
 
-	delete gameScene;
-	gameScene = nullptr;
+	// 終了処理 (unique_ptrなのでdelete不要)
+	titleScene.reset();
+	gameScene.reset();
 
 	// エンジン終了の処理
 	KamataEngine::Finalize();
